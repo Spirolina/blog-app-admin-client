99cfd274145ff3c12b476aa0232ed93b8635c4a8
GÜNER ARDA IŞIK <l5819058@std.yildiz.edu.tr>
2022-05-11 20:30:14 +0300

    add basename

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   
   return (
     <div className="App">
-  <BrowserRouter>
+  <BrowserRouter basename='/blog-app-admin-client'>
         <Navbar />
         <div className='page'>
 
